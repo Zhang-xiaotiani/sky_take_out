4905f7255f9270d03cc2ee78866956f32bda3861
zzzzt <16653959692@163.com>
2023-07-21 09:32:59 +0800

    文件上传到阿里云oss

---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,32 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @ClassName OssConfiguration.java
+ * @Description 配置类，用于创建aliossUtils对象
+ * @createTime 2023年07月20日 19:43:00
+ */
+@Configuration
+@Slf4j
+public class OssConfiguration {
+
+    //启动时就调用
+    @Bean
+    @ConditionalOnMissingBean
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties) {
+        log.info("开始创建阿里云文件上传工具类对象：{}",aliOssProperties);
+        return new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+    }
+}
